509afc427e98ad966416964ee0c93d9f55656bcd
anton2026 <Abraham1@gamca.sk>
2025-09-21 19:38:04 +0200

    Main menu update

---
+++ src/scenes_and_scripts/ui/main.gd
@@ -0,0 +1,26 @@
+extends Control
+class_name MainMenu
+
+
+@export var level_selection_layer: CanvasLayer
+@export var ui_layer: CanvasLayer
+@export var main_menu: Menu
+
+
+func _ready() -> void:
+	Helpers.main_menu = self
+	pause()
+
+
+func play() -> void:
+	level_selection_layer.process_mode = Node.PROCESS_MODE_PAUSABLE
+	ui_layer.process_mode = Node.PROCESS_MODE_DISABLED
+
+func pause() -> void:
+	main_menu.open()
+	level_selection_layer.process_mode = Node.PROCESS_MODE_DISABLED
+	ui_layer.process_mode = Node.PROCESS_MODE_PAUSABLE
+
+
+func _on_main_menu_closed() -> void:
+	play()
---
+++ src/scenes_and_scripts/ui/menus/menu.gd
@@ -0,0 +1,26 @@
+extends Control
+class_name Menu
+
+
+@export var default_focus: Control
+@export var animation_player: AnimationPlayer
+
+signal animation_finished
+signal closed
+
+
+func _ready() -> void:
+	animation_player.animation_finished.connect(_on_animation_player_animation_finished)
+
+
+func open() -> void:
+	animation_player.play("menu_open")
+	default_focus.grab_focus()
+
+func close() -> void:
+	animation_player.play_backwards("menu_open")
+	await animation_finished
+	closed.emit()
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	animation_finished.emit()
