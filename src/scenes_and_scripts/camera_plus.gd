a1bbe854b9f4c304becc5f2c6a39fb5629dd21ab
anton2026 <Abraham1@gamca.sk>
2025-10-10 21:54:42 +0200

    Fixed camera going off bounds in "after death" mode

--- src/scenes_and_scripts/camera_plus.gd
+++ src/scenes_and_scripts/camera_plus.gd
@@ -28,14 +28,14 @@ func _process(delta: float) -> void:
 		var x_before: float = global_position.x
 		global_position.x = move_toward(global_position.x, follow.global_position.x, follow_speed.x * delta)
 		global_position.y = move_toward(global_position.y, follow.global_position.y, follow_speed.y * delta)
-		if Vector2i.UP in limits:
-			global_position.y = max(global_position.y, limits[Vector2i.UP])
-		if Vector2i.DOWN in limits:
-			global_position.y = min(global_position.y, limits[Vector2i.DOWN])
-		if Vector2i.LEFT in limits:
-			global_position.x = max(global_position.x, limits[Vector2i.LEFT])
-		if Vector2i.RIGHT in limits:
-			global_position.x = min(global_position.x, limits[Vector2i.RIGHT])
+	if Vector2i.UP in limits:
+		global_position.y = max(global_position.y, limits[Vector2i.UP])
+	if Vector2i.DOWN in limits:
+		global_position.y = min(global_position.y, limits[Vector2i.DOWN])
+	if Vector2i.LEFT in limits:
+		global_position.x = max(global_position.x, limits[Vector2i.LEFT])
+	if Vector2i.RIGHT in limits:
+		global_position.x = min(global_position.x, limits[Vector2i.RIGHT])
 	
 	shake_value = lerpf(shake_value, 0, shake_fade * delta)
 	offset = Vector2(randf_range(-shake_value, shake_value), randf_range(-shake_value, shake_value))
