fe929e9eb6a404ec4597b79b8956f159b227ec40
anton2026 <Abraham1@gamca.sk>
2025-10-12 11:16:30 +0200

    Pickupable rocks audio pitch randomizing and pickup particles

--- src/scenes_and_scripts/world/objects/pickupable_rock.gd
+++ src/scenes_and_scripts/world/objects/pickupable_rock.gd
@@ -7,15 +7,21 @@ class_name PickupableRock
 
 @onready var area_2d: Area2D = $Area2D
 @onready var pickup_audio: AudioStreamPlayer2D = $PickupAudio
+@onready var pickup_particles: CPUParticles2D = $PickupParticles
+@onready var point_light_2d: PointLight2D = $PointLight2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body is Player and visible:
-		visible = false
+	if body is Player and self_modulate.a > 0:
+		self_modulate.a = 0
+		point_light_2d.visible = false
 		var text: FloatingText = Helpers.create_floating_text(get_parent(), ("+" if amount >= 0 else "") + str(int(amount)) + " R", position + Vector2(0, -24), text_color, -10)
 		text.process_mode = Node.PROCESS_MODE_ALWAYS
 		Settings.collect_rocks(amount)
 		pickup_audio.play()
-		await pickup_audio.finished
+		pickup_audio.pitch_scale = randf_range(0.7, 1.3)
+		pickup_particles.emitting = true
+		while pickup_audio.playing or pickup_particles.emitting:
+			await get_tree().process_frame
 		get_parent().remove_child(self)
 		queue_free()
