9cdf1d3d706ba0d80afe9071204aa21b248cd619
anton2026 <Abraham1@gamca.sk>
2025-10-13 19:18:28 +0200

    Added fog to all levels

---
+++ src/scenes_and_scripts/world/parallax_fogs_manager.gd
@@ -0,0 +1,32 @@
+@tool
+extends Node
+class_name ParallaxFogsManager
+
+
+@export_tool_button("Make Unique") var make_unique_btn: Callable = make_unique
+@export_tool_button("Update") var update_btn: Callable = update
+@export var fog_layers: Array[CanvasItem]
+@export_range(0, 2, 0.01) var fog_intensity: float = 1.0
+@export_range(0, 2, 0.01) var scroll_scale: float = 1.0
+
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+
+func update() -> void:
+	for fog: CanvasItem in fog_layers:
+		if not fog.material is ShaderMaterial:
+			continue
+		fog.material.set_shader_parameter("fog_intensity", fog_intensity)
+		fog.material.set_shader_parameter("scroll_scale", scroll_scale)
+
+func make_unique() -> void:
+	for fog: CanvasItem in fog_layers:
+		if not fog.material:
+			continue
+		fog.material = fog.material.duplicate()
+	print("ADADSSA")
