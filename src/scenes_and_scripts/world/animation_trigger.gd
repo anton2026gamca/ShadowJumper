14289e14056f99ba60fe3d6a31461ab1f082494a
anton2026 <Abraham1@gamca.sk>
2025-10-24 21:13:03 +0200

    Change environment change to be before forest

--- src/scenes_and_scripts/world/animation_trigger.gd
+++ src/scenes_and_scripts/world/animation_trigger.gd
@@ -3,7 +3,7 @@ extends Node2D
 class_name AnimationTrigger
 
 
-@export var enable: bool = true
+@export var enabled: bool = true
 
 @export var animation_tree: AnimationTree
 @export var transitions: Array[AnimationTriggerTransition] = []
@@ -25,17 +25,28 @@ func _process(delta: float) -> void:
 		last_pos = source.position
 
 func update() -> void:
-	if not enable:
-		current_transition = 0
+	current_transition = 0
+	if not enabled:
 		return
 	for i: int in len(transitions):
 		var area: Node = get_node(transitions[i].area)
 		if area is CollisionShape2D and area.shape is RectangleShape2D:
 			var left: float = area.position.x - area.shape.size.x / 2.0
 			var right: float = left + area.shape.size.x
-			if source.position.x >= left and source.position.x <= right:
+			if source.position.x <= right:
 				current_transition = i + 1
-			var value: float = clamp((source.position.x - left) / area.shape.size.x * 2.0 - 1.0, -1.0, 1.0)
+				var value: float = clamp((source.position.x - left) / area.shape.size.x * 2.0 - 1.0, -1.0, 1.0)
+				print(value, "A")
+				var property: StringName = "parameters/" + transitions[i].blend_space_path + "/blend_position"
+				animation_tree.set(property, value)
+				break
+	if current_transition == 0 and len(transitions) > 0:
+		var i: int = len(transitions) - 1
+		var area: Node = get_node(transitions[i].area)
+		if area is CollisionShape2D and area.shape is RectangleShape2D:
+			current_transition = i + 1
+			var left: float = area.position.x - area.shape.size.x / 2.0
+			var right: float = left + area.shape.size.x
 			var property: StringName = "parameters/" + transitions[i].blend_space_path + "/blend_position"
+			var value: float = clamp((source.position.x - left) / area.shape.size.x * 2.0 - 1.0, -1.0, 1.0)
 			animation_tree.set(property, value)
-			#print(transitions[i].blend_space_path, ": ", value, " | ", current_transition)
