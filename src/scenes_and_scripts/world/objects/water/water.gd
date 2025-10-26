421195bd3233154e7d5ce20e23fbec9ca124576e
anton2026 <Abraham1@gamca.sk>
2025-10-26 17:23:22 +0100

    Add water

---
+++ src/scenes_and_scripts/world/objects/water/water.gd
@@ -0,0 +1,29 @@
+@tool
+extends Node2D
+class_name Water
+
+
+@export var lightning_interval: float = 2
+
+@onready var sprite: AnimatedSprite2D = $Sprite
+@onready var lightning_sprite: AnimatedSprite2D = $LightningSprite
+@onready var lightning_sound_effect: AudioStreamPlayer2D = $LightningSoundEffect
+
+
+
+func _ready() -> void:
+	get_tree().create_timer(randf_range(0, lightning_interval), false).timeout.connect(_emit_lightning)
+	sprite.play("default")
+	sprite.frame = randi_range(0, 9)
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _emit_lightning() -> void:
+	lightning_sprite.speed_scale = randf_range(1.0, 2.0)
+	lightning_sprite.play("default")
+	if not Engine.is_editor_hint():
+		lightning_sound_effect.pitch_scale = randf_range(0.75, 1.25)
+		lightning_sound_effect.play()
+	get_tree().create_timer(lightning_interval, false).timeout.connect(_emit_lightning)
