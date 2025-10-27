e402dcd3c88be732180b0859eeeb460c77cb2811
anton2026 <Abraham1@gamca.sk>
2025-10-27 18:04:55 +0100

    Improve water

--- src/scenes_and_scripts/world/objects/water/water.gd
+++ src/scenes_and_scripts/world/objects/water/water.gd
@@ -1,20 +1,18 @@
-@tool
 extends Node2D
 class_name Water
 
 
 @export var lightning_interval: float = 2
 
-@onready var sprite: AnimatedSprite2D = $Sprite
 @onready var lightning_sprite: AnimatedSprite2D = $LightningSprite
 @onready var lightning_sound_effect: AudioStreamPlayer2D = $LightningSoundEffect
+@onready var point_light: PointLight2D = $PointLight2D
 
 
 
 func _ready() -> void:
 	get_tree().create_timer(randf_range(0, lightning_interval), false).timeout.connect(_emit_lightning)
-	sprite.play("default")
-	sprite.frame = randi_range(0, 9)
+	lightning_sprite.animation_finished.connect(func() -> void: point_light.visible = false)
 
 func _process(delta: float) -> void:
 	pass
@@ -23,6 +21,7 @@ func _process(delta: float) -> void:
 func _emit_lightning() -> void:
 	lightning_sprite.speed_scale = randf_range(1.0, 2.0)
 	lightning_sprite.play("default")
+	point_light.visible = true
 	if not Engine.is_editor_hint():
 		lightning_sound_effect.pitch_scale = randf_range(0.75, 1.25)
 		lightning_sound_effect.play()
