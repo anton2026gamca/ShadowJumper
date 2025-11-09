c5679fd8e26c77b211940130f50d0d94a0d6b048
anton2026 <Abraham1@gamca.sk>
2025-11-09 05:34:44 +0100

    Fix language changing button

--- src/scenes_and_scripts/ui/menus/main_menu/main_menu.gd
+++ src/scenes_and_scripts/ui/menus/main_menu/main_menu.gd
@@ -6,6 +6,14 @@ class_name MainMenu
 @export var settings_menu: Menu
 @export var controls_menu: Menu
 
+@onready var lang_to_btn: Dictionary[String, Button] = {
+	"en": $ControlToggleLanguage/HBoxContainer/LanguageEnglish,
+	"ja": $ControlToggleLanguage/HBoxContainer/LanguageJapanese,
+}
+
+
+func _ready() -> void:
+	change_language("en")
 
 func _on_settings_pressed() -> void:
 	open_sub_menu(settings_menu)
@@ -13,5 +21,10 @@ func _on_settings_pressed() -> void:
 func _on_controls_pressed() -> void:
 	open_sub_menu(controls_menu)
 
-func _on_toggle_language_pressed() -> void:
-	TranslationServer.set_locale("en" if TranslationServer.get_locale() == "ja" else "ja")
+func change_language(lang: String) -> void:
+	TranslationServer.set_locale(lang)
+	for btn: Button in lang_to_btn.values():
+		btn.disabled = false
+	if not lang in lang_to_btn:
+		return
+	lang_to_btn[lang].disabled = true
